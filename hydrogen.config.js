49258bf80f9cc22136220ad72840a12837f403f1
HeyImSayem <104578613+HeyImSayem@users.noreply.github.com>
2022-07-10 15:30:13 -0700

    Add files via upload

---
+++ hydrogen.config.js
@@ -0,0 +1,18 @@
+import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
+
+export default defineConfig({
+  shopify: {
+    defaultCountryCode: 'US',
+    defaultLanguageCode: 'EN',
+    storeDomain: 'hydrogen-preview.myshopify.com', //insert store code here
+    storefrontToken: '3b580e70970c4528da70c98e097c2fa0', //insert store api code here
+    storefrontApiVersion: '2022-07',
+  },
+  session: CookieSessionStorage('__session', {
+    path: '/',
+    httpOnly: true,
+    secure: import.meta.env.PROD,
+    sameSite: 'Strict',
+    maxAge: 60 * 60 * 24 * 30,
+  }),
+});
